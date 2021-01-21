5db882eb4d6aac59a071d9595ad93e1368178b41
Duncan McKee <duncan@mckee.net>
2021-01-21 01:13:23 -0500

    Concrete Implementation Testing

--- controller/ConcreteSelectionBehavior.java
+++ controller/ConcreteSelectionBehavior.java
@@ -1,11 +1,16 @@
 package controller;
+import java.util.List;
+
 import models.Machine;
 import models.Order;
 
 public class ConcreteSelectionBehavior implements MachineSelectionBehavior {	
 	@Override
 	public Machine selectMachine(Order order) {
-		// TODO Auto-generated method stub
+		List<Machine> machines = DatabaseConnection.getMachinesForOrder(order);
+		if(machines.size() > 0) {
+			return machines.get(0);
+		}
 		return null;
 	}
 
