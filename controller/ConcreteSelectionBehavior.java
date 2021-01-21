0433634f158eb14728668c34c40db070a548091d
Duncan McKee <duncan@mckee.net>
2021-01-21 01:14:12 -0500

    Temp fix

--- controller/ConcreteSelectionBehavior.java
+++ controller/ConcreteSelectionBehavior.java
@@ -7,7 +7,7 @@ import models.Order;
 public class ConcreteSelectionBehavior implements MachineSelectionBehavior {	
 	@Override
 	public Machine selectMachine(Order order) {
-		List<Machine> machines = DatabaseConnection.getMachinesForOrder(order);
+		List<Machine> machines = new ArrayList<Machine>();//DatabaseConnection.getMachinesForOrder(order);
 		if(machines.size() > 0) {
 			return machines.get(0);
 		}
