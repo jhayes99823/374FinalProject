7ea6c8faeb637b102c687746299f477db4e1b3bb
Sutidamus <koriasutibun@gmail.com>
2021-01-21 00:28:17 -0500

    RequestManager logic handled
    missing ControllerManager call

--- RequestManager.java
+++ RequestManager.java
@@ -1,17 +1,24 @@
 
 public class RequestManager {
 	MachineSelectionBehavior selectionBehavior;
-	JSONManager jsonManager;
 	NotificationManager notificationManager;
-	
+
 	public String handleRequest(String json) {
-		return null;
+		Order order = JSONManager.parseOrderInput(json);
+		Machine machine = performSelection(order);
+		// Communicate with ControllerManager
+		// to send request to machine and get
+		// response back
+		String controllerRespString = null; // = result of ControllerManager call
+		ControllerResponse controllerResponse = JSONManager.parseControllerResponse(controllerRespString);
+		String appResponse = JSONManager.createAppResponse(controllerResponse, machine);
+		return appResponse;
 	}
-	
+
 	public Machine performSelection(Order order) {
-		return null;
+		return selectionBehavior.selectMachine(order);
 	}
-	
+
 	public void setSelectionBehavior(MachineSelectionBehavior selectionBehavior) {
 		this.selectionBehavior = selectionBehavior;
 	}
