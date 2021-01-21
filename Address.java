274727dce9500f27ea977797e41763b1f3800c59
Sutidamus <koriasutibun@gmail.com>
2021-01-20 23:04:10 -0500

    Getters & Constructors for concept classes
    Constructors and getters for the listed files

--- Address.java
+++ Address.java
@@ -2,4 +2,17 @@
 public class Address {
 	private String street;
 	private int ZIP;
+	
+	public Address(String street, int ZIP) {
+		this.street = street;
+		this.ZIP = ZIP;
+	}
+	
+	public String getStreet() {
+		return this.street;
+	}
+	
+	public int getZIP() {
+		return this.ZIP;
+	}
 }
