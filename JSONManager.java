b9f0f4978390c3a19bab35449f04ec122c9bfc90
Duncan McKee <duncan@mckee.net>
2021-01-20 23:02:20 -0500

    Parse orderid

--- JSONManager.java
+++ JSONManager.java
@@ -1,10 +1,13 @@
-import org.json.*;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
 public class JSONManager {
 
 	public Order parseOrderInput(String json) {
-		
-		return null;
+		JSONObject jsonObject = (JSONObject) JSONValue.parse(json);
+		jsonObject = (JSONObject) jsonObject.get("order");
+		Order order = new Order((long)jsonObject.get("orderID"));
+		return order;
 	}
 	
 	public ControllerResponse parseControllerResponse(String json) {
@@ -16,5 +19,22 @@ public class JSONManager {
 	
 	public String createAppResponse(ControllerResponse controllerResponse, Machine machine) {
 		return null;
+	}
+	
+	public static void main(String args[]) {
+		JSONManager manager = new JSONManager();
+		System.out.println(manager.parseOrderInput("{\r\n" + 
+				"  \"order\": { \"orderID\": 1,\r\n" + 
+				"            \"address\": {\r\n" + 
+				"                  \"street\": \"200 N Main\",\r\n" + 
+				"                  \"ZIP\": 47803\r\n" + 
+				"            },\r\n" + 
+				"            \"drink\": \"Americano\",\r\n" + 
+				"            \"condiments\": [\r\n" + 
+				"                {\"name\": \"Sugar\", \"qty\": 1},\r\n" + 
+				"                {\"name\": \"Cream\", \"qty\": 2}\r\n" + 
+				"            ]\r\n" + 
+				"            }\r\n" + 
+				"}").getOrderID());
 	}
 }
--- Order.java
+++ Order.java
@@ -1,8 +1,16 @@
 import java.util.List;
 
 public class Order {
-	private int orderID;
+	private long orderID;
 	private Address address;
 	private String drink;
 	private List<Condiment> condiments;
+	
+	public Order(long orderID /*, Address address, String drink, List<Condiment> condiments */) {
+		this.orderID = orderID;
+	}
+	
+	public long getOrderID() {
+		return this.orderID;
+	}
 }
