994492f07d16b5d186957c04bd2e1a88b3b01203
Sutidamus <koriasutibun@gmail.com>
2021-01-20 22:32:10 -0500

    I added signatures yahoo
    read above

--- JSONManager.java
+++ JSONManager.java
@@ -1,4 +1,18 @@
 
 public class JSONManager {
 
+	public Order parseOrderInput(String json) {
+		return null;
+	}
+	
+	public ControllerResponse parseControllerResponse(String json) {
+		return null;
+	}
+	public String createCommmandStream(Order order, Machine machine) {
+		return null;
+	}
+	
+	public String createAppResponse(ControllerResponse controllerResponse, Machine machine) {
+		return null;
+	}
 }
--- RequestManager.java
+++ RequestManager.java
@@ -1,4 +1,18 @@
 
 public class RequestManager {
-
+	MachineSelectionBehavior selectionBehavior;
+	JSONManager jsonManager;
+	NotificationManager notificationManager;
+	
+	public String handleRequest(String json) {
+		return null;
+	}
+	
+	public Machine performSelection(Order order) {
+		return null;
+	}
+	
+	public void setSelectionBehavior(MachineSelectionBehavior selectionBehavior) {
+		
+	}
 }
