c85292bfa3722ec1ec7bb9b0af7a9ee3c13c00f9
Duncan McKee <duncan@mckee.net>
2021-01-20 22:25:35 -0500

    add fields

--- ControllerResponse.java
+++ ControllerResponse.java
@@ -1,4 +1,7 @@
 
 public class ControllerResponse {
-
+	private int orderID;
+	private int status;
+	private String errordesc;
+	private int errorcode;
 }
--- Order.java
+++ Order.java
@@ -1,4 +1,8 @@
+import java.util.List;
 
 public class Order {
-
+	private int orderID;
+	private Address address;
+	private String drink;
+	private List<Condiment> condiments;
 }
