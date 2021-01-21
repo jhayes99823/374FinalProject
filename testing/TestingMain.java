15f80f14631c115c429228e84c92b7cb45156135
Duncan McKee <duncan@mckee.net>
2021-01-21 01:21:06 -0500

    Update testing class

--- testing/TestingMain.java
+++ testing/TestingMain.java
@@ -4,22 +4,47 @@ import controller.RequestManager;
 
 public class TestingMain {
 
+	private static final String[] jsons = {
+		"{\r\n" + 
+		"  \"order\": { \"orderID\": 1,\r\n" + 
+		"            \"address\": {\r\n" + 
+		"                  \"street\": \"200 N Main\",\r\n" + 
+		"                  \"ZIP\": 47803\r\n" + 
+		"            },\r\n" + 
+		"            \"drink\": \"Americano\",\r\n" + 
+		"            \"condiments\": [\r\n" + 
+		"                {\"name\": \"Sugar\", \"qty\": 1},\r\n" + 
+		"                {\"name\": \"Cream\", \"qty\": 2}\r\n" + 
+		"            ]\r\n" + 
+		"            }\r\n" + 
+		"}",
+		"{\r\n" + 
+		"  \"order\": { \"orderID\": 2,\r\n" + 
+		"            \"address\": {\r\n" + 
+		"                  \"street\": \"200 N Main\",\r\n" + 
+		"                  \"ZIP\": 47803\r\n" + 
+		"            },\r\n" + 
+		"            \"drink\": \"Expresso\"\r\n" + 
+		"            }\r\n" + 
+		"}",
+		"{\r\n" + 
+		"  \"order\": { \"orderID\": 3,\r\n" + 
+		"            \"address\": {\r\n" + 
+		"                  \"street\": \"200 N Main\",\r\n" + 
+		"                  \"ZIP\": 47803\r\n" + 
+		"            },\r\n" + 
+		"            \"drink\": \"Pumpkin Spice\",\r\n" + 
+		"            \"condiments\": [\r\n" + 
+		"                {\"name\": \"Cream\", \"qty\": 1}\r\n" + 
+		"            ]\r\n" + 
+		"            }\r\n" + 
+		"}"
+	};
+	
 	public static void main(String[] args) {
 		RequestManager manager = new RequestManager();
 		manager.setSelectionBehavior(new ConcreteSelectionBehavior());
-		String response = manager.handleRequest("{\r\n" + 
-				"  \"order\": { \"orderID\": 1,\r\n" + 
-				"            \"address\": {\r\n" + 
-				"                  \"street\": \"200 N Main\",\r\n" + 
-				"                  \"ZIP\": 47803\r\n" + 
-				"            },\r\n" + 
-				"            \"drink\": \"Americano\",\r\n" + 
-				"            \"condiments\": [\r\n" + 
-				"                {\"name\": \"Sugar\", \"qty\": 1},\r\n" + 
-				"                {\"name\": \"Cream\", \"qty\": 2}\r\n" + 
-				"            ]\r\n" + 
-				"            }\r\n" + 
-				"}");
+		String response = manager.handleRequest(jsons[0]);
 		System.out.println(response);
 	}
 
