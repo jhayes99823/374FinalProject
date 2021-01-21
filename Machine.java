a6d5c42099a72a34d4d3207ea6becaa2d00a2a0f
Sutidamus <koriasutibun@gmail.com>
2021-01-20 23:14:51 -0500

    Added getters and contructor to Machine

--- Machine.java
+++ Machine.java
@@ -4,4 +4,22 @@ public class Machine {
 	private int id;
 	private CoffeeController controller;
 	private List<Capability> capabilities;
+	
+	public Machine(int id, CoffeeController controller, List<Capability> capabilities) {
+		this.id = id;
+		this.controller = controller;
+		this.capabilities = capabilities;
+	}
+	
+	public int getID() {
+		return this.id;
+	}
+	
+	public CoffeeController getController() {
+		return this.controller;
+	}
+	
+	public List<Capability> getCapabilities(){
+		return this.capabilities;
+	}
 }
