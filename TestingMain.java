a3fc06214142d74a30a2874c6ff4f321a4a26096
Duncan McKee <duncan@mckee.net>
2021-01-21 01:02:47 -0500

    Add a testing file

---
+++ TestingMain.java
@@ -0,0 +1,23 @@
+
+public class TestingMain {
+
+	public static void main(String[] args) {
+		RequestManager manager = new RequestManager();
+		manager.setSelectionBehavior(new ConcreteSelectionBehavior());
+		String response = manager.handleRequest("{\r\n" + 
+				"  \"order\": { \"orderID\": 1,\r\n" + 
+				"            \"address\": {\r\n" + 
+				"                  \"street\": \"200 N Main\",\r\n" + 
+				"                  \"ZIP\": 47803\r\n" + 
+				"            },\r\n" + 
+				"            \"drink\": \"Americano\",\r\n" + 
+				"            \"condiments\": [\r\n" + 
+				"                {\"name\": \"Sugar\", \"qty\": 1},\r\n" + 
+				"                {\"name\": \"Cream\", \"qty\": 2}\r\n" + 
+				"            ]\r\n" + 
+				"            }\r\n" + 
+				"}");
+		System.out.println(response);
+	}
+
+}
