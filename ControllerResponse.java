464523ed24cb1797d56c005ad0bf5b12e0094dbe
Sutidamus <koriasutibun@gmail.com>
2021-01-20 23:08:45 -0500

    Update ControllerResponse.java

--- ControllerResponse.java
+++ ControllerResponse.java
@@ -4,4 +4,20 @@ public class ControllerResponse {
 	private int status;
 	private String errordesc;
 	private int errorcode;
+	
+	public int getOrderID() {
+		return this.orderID;
+	}
+	
+	public int getStatus() {
+		return this.status;
+	}
+	
+	public String getErrorDesc() {
+		return this.errordesc;
+	}
+	
+	public int getErrorCode() {
+		return this.errorcode;
+	}
 }
