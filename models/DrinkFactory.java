fc57ddbe1e30fabd70b6f675a900cd00b23fc990
Jordan Hayes <jordanhayes@jordans-mbp.wlan.rose-hulman.edu>
2021-02-03 23:54:23 -0500

    add factories

---
+++ models/DrinkFactory.java
@@ -0,0 +1,16 @@
+package models;
+
+import java.util.List;
+
+import org.json.simple.JSONObject;
+
+import controller.Database;
+import recipes.Recipe;
+
+public abstract class DrinkFactory {
+	public static Recipe getRecipe(JSONObject drinkObj) {
+		List<Recipe> recipes = Database.getInstance().getRecipe(drinkObj);
+		
+		return null;
+	}
+}
